251c102af820c15f36042ce309bc770cea031126
Vipin Girjapure <vipingirjapure114@gmail.com>
2022-08-05 21:34:00 +0530

    first

---
+++ src/Components/DigitButton.jsx
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./Main";
+
+const DigitButton = ({dispatch,digit})=>{
+    return <button onClick={()=>dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}>{digit}</button>
+}
+
+export default DigitButton;
